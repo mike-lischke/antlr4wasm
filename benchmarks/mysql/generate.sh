8b3f70520af504d0fbe31bbc90d1c0d5c3f352ca
Mike Lischke <mike.lischke@oracle.com>
2023-08-16 12:48:31 +0200

    Preparation of benchmarking
    Added MySQL grammar + support classes. Generated TS + C++ classes using latest ANTLR4. This is the initial check-in of these files to better see the required changes in the generation of the TS files.

---
+++ benchmarks/mysql/generate.sh
@@ -0,0 +1,13 @@
+# Generates the MySQL parser + lexer files for the benchmarks.
+
+java -jar ../../antlr/antlr-4.13.0-complete.jar \
+    -Dlanguage=Cpp -o ./generated/cpp \
+    -no-visitor -no-listener -package antlr4 \
+    -Xexact-output-dir -Werror \
+    MySQLLexer.g4 MySQLParser.g4
+
+java -jar ../../antlr/antlr-4.13.0-complete.jar \
+    -Dlanguage=TypeScript -o ./generated/TypeScript \
+    -no-visitor -no-listener -package antlr4 \
+    -Xexact-output-dir -Werror \
+    MySQLLexer.g4 MySQLParser.g4
--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,10 +4,10 @@ java -jar tests/antlr-4.13.0-complete.jar \
     -Dlanguage=Cpp -o tests/generated/cpp \
     -no-visitor -no-listener -package antlr4 \
     -Xexact-output-dir -Werror \
-    tests/grammars/Whitebox.g4 tests/grammars/CPP14.g4 tests/grammars/Expr.g4
+    tests/grammars/Whitebox.g4 # tests/grammars/CPP14.g4 tests/grammars/Expr.g4
 
 java -jar tests/antlr-4.13.0-complete.jar \
     -Dlanguage=TypeScript -o tests/generated/TypeScript \
     -no-visitor -no-listener -package antlr4 \
     -Xexact-output-dir -Werror \
-    tests/grammars/Whitebox.g4 tests/grammars/CPP14.g4 tests/grammars/Expr.g4
+    tests/grammars/Whitebox.g4 # tests/grammars/CPP14.g4 tests/grammars/Expr.g4
